a2268998be65e70721c78b134da49d6c84e154f1
ahammadshawki8 <ahammadshawki8@gmail.com>
2020-09-08 13:40:01 +0600

    new

--- CHAPTER 08 (trees)/binary_layout.py
+++ CHAPTER 08 (trees)/binary_layout.py
@@ -1,4 +1,76 @@
-from binary_eular_tour import *
+# Extra Utilities
+
+class EulerTour:
+    """Abstract base class for performing Euler tour of a tree.
+
+    _hook_previsit ans _hook_postvisit may be overriden by subclasses.
+    """
+    def __init__(self,tree):
+        """Prepare an Euler tour template for given tree."""
+        self._tree = tree
+
+    def tree(self):
+        """Return reference to the tree being traversed."""
+        return self._tree
+
+    def execute(self):
+        """Perform the tour and return any result from post visit of root."""
+        if len(self._tree) > 0:
+            return self._tour(self._tree.root(),0,[])   # start the recursion
+
+    def _tour(self,p,d,path):
+        """Perform tour of subtree rooted at Position p.
+
+        p      Position of current node being visited
+        d      depth of p in the tree
+        path   list of indices of children on path from root to p
+        """
+        self._hook._previsit(p,d,path)                  # pre-visit p
+        result = []
+        path.append(0)                  # add new index to the end of path before recursion
+        for c in self._tree.children(p):
+            results.append(self._tour(c,d+1,path))      # recur on child's subtree
+            path[-1] += 1           # increment index
+        path.pop()                  # remove extraneous index from the end of the path
+        answer = self._hook._postvisit(p,d,path,results)    # post-visit p
+        return answer
+
+    def _hook_previsit(self,p,d,path):          # can be overriden
+        pass
+    def _hook_postvisit(self,p,d,path,results): # can be overriden
+        pass
+
+class BinaryEularTour(EulerTour):
+    """Abstract base class for performing Eular tour of a binary tree.
+
+    This version includes an additional _hook_invisit that is called after the tour
+    of the left subtree (if any), yet before the tour of right subtree (if any).
+
+    NOTE: Right child is always assigned index 1 in path, even if no left sibling.
+    """
+    def _tour(self,p,d,path):
+        results = [None,None]               # will update the results of recursions
+        self._hook_previsit(p,d,path)       # pre visit for p
+        if self._tree.left(p) is not None:  # consider the left child
+            path.append(0)
+            result[0] = self._tour(self._tree.left(p), d+1, path)
+            path.pop()
+        self._hook_invisit(p,d,path)        # in visit for p
+        if self._tree.right(p) is not None: # consider the right child
+            path.append(1)
+            result[1] = self._tour(self._tree.right(p), d+1, path)
+            path.pop()
+        answer = self._hook_postvisit(p,d,path,results) # post visit of p
+
+    def _hook_invisit(self,p,d,path):       # can be overriden
+        pass
+        return answer
+
+
+
+
+
+# Main Code
 
 class BinaryLayout(BinaryEularTour):
     """Class for computing (x,y) coordinates for each node of a binary tree."""
--- CHAPTER 08 (trees)/breadth_first_traversals.py
+++ CHAPTER 08 (trees)/breadth_first_traversals.py
@@ -1,7 +1,156 @@
-# the functions of this code belong to Tree class of tree_abc
+# Extra Utilities
+class Tree:
+    """Abstract base class for representing a tree structure."""
 
-from tree_abc import *
-from linked_queue_class import *
+    #---------------------------nested Position class----------------------------
+    class Position:
+        """An abstraction representing the location of a single element."""
+
+        def element(self):
+            """Return the element stored at this Position."""
+            raise NotImplementedError("Must be implemented by subclass")
+
+        def __eq__(self,other):
+            """Return True if other Position represents the same location."""
+            raise NotImplementedError("Must be implemented by subclass")
+
+        def __ne__(self,other):
+            """Return True if other does not represents the same location."""
+            return not(self == other)           # opposite of __eq__
+
+    #-----------abstract methods that contrete subclass bust support-----------
+    def root(self):
+        """Return Position representing the tree's root (or None if empty)."""
+        raise NotImplementedError("Must be implemented by subclass")
+
+    def parent(self,p):
+        """Return Position representing p's parent (or None if p is root)."""
+        raise NotImplementedError("Must be implemented by subclass")
+
+    def num_children(self,p):
+        """Return the number of children the Position p has."""
+        raise NotImplementedError("Must be implemented by subclass")
+
+    def children(self,p):
+        """Generate an iteration of Positions representing p's children."""
+        raise NotImplementedError("Must be implemented by subclass")
+
+    def __len__(self):
+        """Return the total number of elements in the tree."""
+        raise NotImplementedError("Must be implemented by subclass")
+
+    #--------------concrete methods implemented in this class------------------
+    def is_root(self,p):
+        """Return True if Position p representing the root of  the tree."""
+        return (self.root() == p)
+
+    def is_leaf(self,p):
+        "Return True if Position p does not have any children"""
+        return (self.num_children(p) == 0)
+
+    def is_empty(self):
+        """Return True id the tree is empty."""
+        return len(self) == 0
+
+    #-----------------------------new methods----------------------------------
+    def depth(self,p):
+        """Return the number of levels separating Position p from the root."""
+        if self.is_root(p):
+            return 0
+        else:
+            return 1 + self.depth(self.parent(p))
+
+    def _height1(self):             # works, but O(n**2) worst-case
+        """Return the height of the tree."""
+        return max(self.depth(p) for p in self.positions() is self.is_leaf(p))
+
+    def _height2(self,p):           # time is linear in size of sub-tree
+        """Return the height of the subtree rooted at Position p."""
+        if self.is_leaf(p):
+            return 0
+        else:
+            return max(self._height2(c) for c in self.children(p))
+
+    def height(self,p=None):
+        """Return the height of the subtree rooted at Position p.
+
+        If p is None, return the height of the entire tree.
+        """
+        if p is None:
+            p = self.root()
+        return self._height2(p)         # start _height2 recursion
+
+
+# creating linked queue class
+class LinkedQueue:
+    """FIFO queue implementation using a sigly linked list for storage."""
+
+    def __init__(self):
+        """Create an empty queue"""
+        self._head = None
+        self._tail = None
+        self._size = 0                  # number of sequence elements
+
+    def __len__(self):
+        """Return the number of elements in the queue."""
+        return self._size
+
+    def is_empty(self):
+        """Return True if the queue is empty."""
+        return (self._size == 0)
+
+    def first(self):
+        """Return (but do not remove) the element at the front of the queue.
+
+        Raise Empty exception if the queue is empty.
+        """
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        return self._head._element      # front alligned with the head of list
+
+    def dequeue(self):
+        """Remove and return the first element of the queue (i.e., FIFO).
+
+        Raise Empty exception if the queue is empty.
+        """
+        if self.is_empty():
+            raise Empty("Queue is empty")
+        answer = self._head._element
+        self._head = self._head._next
+        self._size -= 1
+        if self.is_empty():             # special case as queue is empty
+            self._tail=None             # removed head had been the tail
+        return answer
+
+    def enqueue(self,e):
+        """Add an element to the back of the queue."""
+        newest = self._Node(e,None)     # node will be new tail node
+        if self.is_empty():             # special case: previously empty
+            self._head = newest
+        else:
+            self._tail._next = newest
+        self.tail = newest              # update reference to tail node
+        self._size += 1
+
+# creating empty class
+class Empty(Exception):
+    pass
+
+# simple implementation of Node class.
+class _Node:
+    """Lightweight, non-public class for storing a singly linked list"""
+    __slots__ = "_element", "_next"         # streamline memory usage.
+
+    def __init__(self,element,next):
+        self._element = element             # reference to user's element
+        self._next = next                   # reference to next node
+
+
+
+
+
+
+# Main Code
 
 class TreeTraversals(Tree):
     def breadthfirst(self):
